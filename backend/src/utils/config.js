dd068a3c0fb7cea6e6e3e72cc023f38074233ee5
thelicato <delicato.angelo@gmail.com>
2020-05-19 11:41:47 +0200

    Removed useless package-lock

--- backend/src/utils/config.js
+++
@@ -1,6 +0,0 @@
-exports.PORT = 5000
-exports.NODE_ENV = "development"
-exports.MONGO_URI = "mongodb+srv://poe:XDrsCNfTx9iOc5Dy@cosmoscluster-ctn6b.mongodb.net/test?retryWrites=true&w=majority"
-exports.JANUS_URI = "wss://janus.conf.meetecho.com/ws/"
-exports.JWT_SECRET = "randomString"
-exports.EVENT_HANDLER_SECRET = "pescapestaizittodiocane"
