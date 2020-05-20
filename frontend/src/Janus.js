e3584a1aad3e9f4505b9f25788591742459e0678
thelicato <delicato.angelo@gmail.com>
2020-05-20 23:22:29 +0200

    Fixed typing error in Janus.js

--- frontend/src/Janus.js
+++ frontend/src/Janus.js
@@ -1,5 +1,5 @@
 var websocketURI;
-if(process.env.NODE_ENV === "development")
+if(process.env.NODE_ENV === "development"){
 	websocketURI = "ws://localhost:8080/";
 }
 else if(process.env.NODE_ENV === "production"){
