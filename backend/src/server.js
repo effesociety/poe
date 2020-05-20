fc572ea3f1845d3c0bd621756197fd6627c55ec8
thelicato <delicato.angelo@gmail.com>
2020-05-20 23:05:26 +0200

    Added path import

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 const apiRouter = express.Router()
 const mongo = require('./utils/db')
 const userRoutes = require('./routes/auth-api')
